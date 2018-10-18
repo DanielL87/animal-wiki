7a9c65c667850d2db8efdbeed275af9e9f9b95bc
Daniel Lira and Edin Filipovic <edin@>
2018-10-18 09:53:28 -0700

    setup styles,js and html, created divs for each image

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $("#turtle").click(function() {
+    $("p").hide();
+    $(".turtle").show();
+  });
+
+  $("#cobra").click(function() {
+    $("p").hide();
+    $(".cobra").show();
+
+  });
+  
+  $("#hornet").click(function() {
+    $("p").hide();
+    $(".hornet").show();
+  });
+});
